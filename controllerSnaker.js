be12129a28cd1ef89e148f815ed99906f9ffd625
BaptFR <baptistesalazar@gmail.com>
2025-05-15 15:46:06 +0200

    Feat: structure base

--- controllerSnaker.js
+++ controllerSnaker.js
@@ -1,4 +1,16 @@
 
-let snaket = document.getElementById('snake');
+let snake = document.getElementById('snake');
 
+function move () {
 let positionSnake = 0;
+    for (let i=0; i<delay; i++){
+
+        positionSnake += 10;
+        snake.style.top = positionSnake + "px";
+    
+
+
+    }
+}
+
+setTimeout(move, 5000);
