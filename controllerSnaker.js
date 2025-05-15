2f07852d8d68be859bf0e4f36a93cfcd2582a51c
BaptFR <baptistesalazar@gmail.com>
2025-05-15 16:38:33 +0200

    Feat: gestion  mouvements

--- controllerSnaker.js
+++ controllerSnaker.js
@@ -1,16 +1,53 @@
 
 let snake = document.getElementById('snake');
-
-function move () {
 let positionSnake = 0;
-    for (let i=0; i<delay; i++){
 
+setInterval(moveSnake, 100);
+
+
+//Fonctions de mouvement
+function moveSnakeDown () {
         positionSnake += 10;
-        snake.style.top = positionSnake + "px";
-    
-
-
-    }
+        snake.style.top= positionSnake + "px";
+}
+function moveSnakeUp () {
+        positionSnake -= 10;
+        snake.style.top= positionSnake + "px";
+}
+function moveSnakeRight () {
+        positionSnake += 10;
+        snake.style.left= positionSnake + "px";
+}
+function moveSnakeLeft () {
+        positionSnake -= 10;
+        snake.style.left= positionSnake + "px";
 }
 
-setTimeout(move, 5000);
+
+
+//Gestionnaires d'evenements 
+    //Listener pour touches
+document.addEventListener('keydown', function(event){
+    if(event.key === 'ArrowRight'){
+       moveSnakeRight;
+       LastkeyDownSave = 'ArrowRight';
+    }
+    if (event.key === 'ArrowLeft'){
+       moveSnakeLeft;
+        LastkeyDownSave = 'ArrowRight';
+    }
+    if (event.key === 'ArrowDown'){
+       moveSnakeDown;
+        LastkeyDownSave = 'ArrowRight';
+    }
+    if (event.key === 'ArrowUp'){
+       moveSnakeUp
+    }
+});
+
+
+
+function snakeEvent (keydown) {
+
+
+}
