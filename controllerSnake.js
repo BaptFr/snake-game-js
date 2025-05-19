1bd05c413af75762235bf096d7c2f6764702f320
BaptFR <baptistesalazar@gmail.com>
2025-05-19 16:49:05 +0200

    Feat: ajout corps

--- controllerSnake.js
+++ controllerSnake.js
@@ -1,77 +1,76 @@
 
 let snake = document.getElementById('snake-head');
-let apple= document.getElementById('apple');
+let apple = document.getElementById('apple');
 let container = document.getElementById('container-jeu');
 let scoreDisplay = document.getElementById('score-display');
-
-let positionSnakeX =  300;
+let positionSnakeX = 300;
 let positionSnakeY = 200;
+let snakeSizePosition = [];
 
 
 //FONCTIONS MOUVEMENT SNAKE
-function moveSnakeDown () {
+function moveSnakeDown() {
         positionSnakeY += 10;
-        if(positionSnakeY >= 590){
+        if (positionSnakeY >= 590) {
                 positionSnakeY = 10;
-                snake.style.top= positionSnakeY + 'px';
+                snake.style.top = positionSnakeY + 'px';
         } else {
-                snake.style.top= positionSnakeY + 'px';
+                snake.style.top = positionSnakeY + 'px';
         }
 }
-function moveSnakeUp () {
+function moveSnakeUp() {
         positionSnakeY -= 10;
-        if(positionSnakeY <= 4){
+        if (positionSnakeY <= 4) {
                 positionSnakeY = 599;
-                snake.style.top= positionSnakeY + 'px';
+                snake.style.top = positionSnakeY + 'px';
         } else {
-                snake.style.top= positionSnakeY + 'px';
+                snake.style.top = positionSnakeY + 'px';
         }
 }
-function moveSnakeRight () {
+function moveSnakeRight() {
         positionSnakeX += 10;
-        if(positionSnakeX >= 590){
+        if (positionSnakeX >= 590) {
                 positionSnakeX = 0;
-                snake.style.left= positionSnakeX + 'px';
+                snake.style.left = positionSnakeX + 'px';
         } else {
-                snake.style.left= positionSnakeX + 'px';
+                snake.style.left = positionSnakeX + 'px';
         }
 }
-function moveSnakeLeft () {
+function moveSnakeLeft() {
         positionSnakeX -= 10;
-        if(positionSnakeX <= 5){
+        if (positionSnakeX <= 5) {
                 positionSnakeX = 599;
-                snake.style.left= positionSnakeX + 'px';
+                snake.style.left = positionSnakeX + 'px';
         } else {
-                snake.style.left= positionSnakeX + 'px';
+                snake.style.left = positionSnakeX + 'px';
         }
 }
 
 
 
 //GESTION D'EVENEMENTS
-
-     //Listener pour touches + renvoi des positions
+//Listener pour touches + renvoi des positions
 let lastkeyDownSave = '';
-let interval =  setInterval(() => 
+let interval = setInterval(() =>
         snakeEvent(lastkeyDownSave), 100);
 
-document.addEventListener('keydown', function(event){
+document.addEventListener('keydown', function (event) {
         lastkeyDownSave = event.key;
         interval;
 });
 
-    //Fonction reaction gestion evenement
+//Fonction reaction gestion evenement
 function snakeEvent(keydown) {
-        if(keydown === 'ArrowRight'){
+        if (keydown === 'ArrowRight') {
                 moveSnakeRight();
         }
-        if (keydown === 'ArrowLeft'){
+        if (keydown === 'ArrowLeft') {
                 moveSnakeLeft();
         }
-        if (keydown === 'ArrowDown'){
+        if (keydown === 'ArrowDown') {
                 moveSnakeDown();
         }
-        if (keydown === 'ArrowUp'){
+        if (keydown === 'ArrowUp') {
                 moveSnakeUp();
         }
 }
@@ -79,51 +78,55 @@ function snakeEvent(keydown) {
 
 let score = 0;
 //CHANGEMENT POSITION APPLE
-function AppleMove(){
-        apple.style.top = Math.floor(Math.random() * 50);
-        apple.style.left = Math.floor(Math.random() * 50);    
+function AppleMove() {
+        apple.style.top = Math.floor(Math.random() * 100);
+        apple.style.left = Math.floor(Math.random() * 100);
 }
 
 
 //POSITIONS DES ELEMENTS en continu 
-let intervalPositions =  setInterval(() => 
+let intervalPositions = setInterval(() =>
         Positions(), 100);
-function Positions () {
+function Positions() {
         let snakePositionDatas = snake.getBoundingClientRect()
-        console.log(snakePositionDatas);
+        //console.log(snakePositionDatas);
         let applePositionDatas = apple.getBoundingClientRect();
-        console.log(applePositionDatas);
-        let gameLimits = container.getBoundingClientRect();
+        //console.log(applePositionDatas);
+        let snakeValorsArray = [snakePositionDatas.x, snakePositionDatas.y];
+        //console.log(snakeValorsArray);
+
         //POMME MANGEE avec tolérance +- 10px
-        if(Math.abs(snakePositionDatas.x - applePositionDatas.x)<=16 && 
-        Math.abs(snakePositionDatas.y - applePositionDatas.y)<=16){
+        if (Math.abs(snakePositionDatas.x - applePositionDatas.x) <= 16 &&
+                Math.abs(snakePositionDatas.y - applePositionDatas.y) <= 15) {
                 AppleMove();
-                score ++;
-                alert('SCORE:  ' + score );
-                scoreDisplay.innerHTML= 'SCORE: ' + score; // REVOIR 
+                score++;
+                alert('SCORE:  ' + score);
+                scoreDisplay.innerHTML = 'SCORE: ' + score; // REVOIR 
+
+                //Tableau coordonnées
+                let snakeSizePositionDats = [];
+
+                //Coordonnées dernier élement du tableau 
+
+                //FONCTION AJOUT SNAKE
                 addSnakeSize(snakePositionDatas);
-                //pomme mangée
-                //position snake à ce moment
-                        //Ajouter dans un tableau
-                        //Mais ajouter les nombre selon le nombre de parties
-                        //utiliser position snake pour ajouter morceau
-                        //(boucle)
-                //appeler fonc qui ajoute
+                //Ajout au tableau des coordonnées du snakeSize au dernier index
         }
 }
 
 
-//TAILLE SNAKE
-function addSnakeSize(snakePosition) {
+//AJOUT SNAKE
+function addSnakeSize(snakePositionDatas) {
+        //test positions 
+        let positionElementX = 0;
+        let positionElementY = 0;
+
+        //Snake grossit
         let snakeSize = document.createElement('div');
         snakeSize.className = 'snake-size';
-        snakeSize.style.top = snakePosition.y + 'px';
-        snakeSize.style.left = snakePosition.x + 'px';
+        snakeSize.style.top = (snakePositionDatas.X + 20) + 'px';
+        snakeSize.style.left = snakePositionDatas.Y + 'px';
         container.appendChild(snakeSize);
+        // snakeSize.style.top = snakePositionDatas.Y + 'px';
+        // snakeSize.style.left = snakePositionDatas.X + 'px';
 }
-
-
-
-
-
-
